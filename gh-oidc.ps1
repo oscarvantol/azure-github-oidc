d4711b2788a450fc34431f2d259e6f73222d10c2
Oscar van Tol <ovantol@gmail.com>
2022-09-14 20:18:35 +0000

    added some flow, made it work in pws/codespaces

--- gh-oidc.ps1
+++ gh-oidc.ps1
@@ -1,6 +1,6 @@
-$AppName = 'replace-with-app-reg-name'
-$GitHubOrg = 'replace-with-your-org'
-$GitHubRepo = 'replace-with-your-repo'
+$AppName = Read-Host "Enter the name of the app registration"
+$GitHubOrg = Read-Host "Enter the name of the GitHub organization or user"
+$GitHubRepo = Read-Host "Enter the name of the Repository"
 
 $GitHubRepoPath = "$GitHubOrg/$GitHubRepo"
 
@@ -17,51 +17,73 @@ if ($AppId){
    Write-Output "App already exists: $AppId"
 } else {
    Write-Output "Creating App"
-   $AppId=$(az ad app create --display-name ${AppName} --query appId -o tsv)
+   $AppId=$(az ad app create --display-name $AppName --query appId -o tsv)
+   Write-Output "App created ${AppId}"
 }
 
 
-$ServicePrincipal=$(az ad sp list --filter "appId eq '$AppId'" --query [].id -o tsv)
-
+$ServicePrincipal=$(az ad sp show --id $AppId --query objectId -o tsv)
 if ($ServicePrincipal) {
    Write-Output "Service Principal already exists: $ServicePrincipal"
 } else {
    Write-Output "Creating Service Principal"
-   $ServicePrincipal=$(az ad sp create --id $AppId --query id -o tsv)
+   $ServicePrincipal=$(az ad sp create --id $AppId --query objectId -o tsv)
    
    Write-Output "Assigning contributor role on subscription!"
    az role assignment create --role contributor --subscription $SubId --assignee-object-id $ServicePrincipal --assignee-principal-type ServicePrincipal
 }
  
-$AppObjectId=$(az ad app show --id $AppId --query id -o tsv)
+$AppObjectId=$(az ad app show --id $AppId --query objectId -o tsv)
 Write-Output "Application ObjectId $AppObjectId"
 
-$currentCredsResult = az rest --method GET --uri "https://graph.microsoft.com/beta/applications/$AppObjectId/federatedIdentityCredentials"
-$currentCredsResult.Value
 
-$prFic = @{
-   "name"= "pull-request"
+Write-Output "Adding federated identity credentials (fic)"
+
+$createPrFic = Read-Host "Do you want to add fic for pull requests? (y/N)"
+if ($createPrFic -eq 'y')
+{
+   $prFic = @{
+      "name"= "pull-request"
+      "issuer"= "https://token.actions.githubusercontent.com"
+      "subject"= "repo:${GitHubRepoPath}:pull_request"
+      "description"= "pull request access"
+      "audiences"= @( "api://AzureADTokenExchange")
+   }
+
+   $prFic = ConvertTo-Json $prFic -Compress
+   $prFic = $prFic -replace "`"", "\`""
+   az rest --method POST --uri "https://graph.microsoft.com/beta/applications/$AppObjectId/federatedIdentityCredentials" --headers "Content-Type=application/json" --body $prFic
+}
+
+$branchName = Read-Host "Create fic for a branch? (type the branch name or press enter to skip)"
+if ($branchName)
+{
+   $branchFic = @{
+   "name"= "branch-${branchName}"
    "issuer"= "https://token.actions.githubusercontent.com"
-   "subject"= "repo:${GitHubRepoPath}:pull_request"
-   "description"= "pull request access"
-   "audiences"= @( "api://AzureADTokenExchange")
+   "subject"= "repo:${GitHubRepoPath}:ref:refs/heads/${branchName}"
+   "description"= "branch access"
+      "audiences"= @( "api://AzureADTokenExchange")
+   }
+   $branchFic = ConvertTo-Json $branchFic -Compress
+   $branchFic = $branchFic -replace "`"", "\`""
+   az rest --method POST --uri "https://graph.microsoft.com/beta/applications/$AppObjectId/federatedIdentityCredentials" --headers "Content-Type=application/json" --body $branchFic
 }
 
-$prFic = ConvertTo-Json $prFic -Compress
-$prFic = $prFic -replace "`"", "\`""
-az rest --method POST --uri "https://graph.microsoft.com/beta/applications/$AppObjectId/federatedIdentityCredentials" --headers "Content-Type=application/json" --body $prFic
-
-$mainBranchFic = @{
-  "name"= "main-branch"
-  "issuer"= "https://token.actions.githubusercontent.com"
-  "subject"= "repo:${GitHubRepoPath}:ref:refs/heads/main"
-  "description"= "main branch access"
-   "audiences"= @( "api://AzureADTokenExchange")
+$envName = Read-Host "Create fic for an environment? (type the environment name or press enter to skip)"
+if ($envName)
+{
+   $envFic = @{
+   "name"= "env-${envName}"
+   "issuer"= "https://token.actions.githubusercontent.com"
+   "subject"= "repo:${GitHubRepoPath}:environment:${envName}"
+   "description"= "environment access - ${envName}"
+      "audiences"= @( "api://AzureADTokenExchange")
+   }
+   $envFic = ConvertTo-Json $envFic -Compress
+   $envFic = $envFic -replace "`"", "\`""
+   az rest --method POST --uri "https://graph.microsoft.com/beta/applications/$AppObjectId/federatedIdentityCredentials" --headers "Content-Type=application/json" --body $envFic
 }
-$mainBranchFic = ConvertTo-Json $mainBranchFic -Compress
-$mainBranchFic = $mainBranchFic -replace "`"", "\`""
-
-az rest --method POST --uri "https://graph.microsoft.com/beta/applications/$AppObjectId/federatedIdentityCredentials" --headers "Content-Type=application/json" --body $mainBranchFic
 
 
 Write-Output "AZURE_CLIENT_ID=$AppId"
